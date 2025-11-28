6be9c8c01e0c97ce989f49789391ccefcf6b2ced
Charles Olinsky <charlesolinsky@gmail.com>
2025-11-27 19:59:53 -0500

    +player animations

--- player.gd
+++ player.gd
@@ -3,15 +3,29 @@ extends CharacterBody2D
 
 func _process(_delta: float):
 	
+	#tackle this later
+	if Input.is_action_pressed("attack"):
+		pass
+		
+	#Movement
 	if Input.is_action_pressed("move_right"):
 		position.x += 1
-	if Input.is_action_pressed("move_left"):
+		$AnimatedSprite2D.play("walk_right_unarmed")
+	elif Input.is_action_pressed("move_left"):
 		position.x -= 1
-	if Input.is_action_pressed("move_down"):
+		$AnimatedSprite2D.play("walk_left_unarmed")
+	elif Input.is_action_pressed("move_down"):
 		position.y += 1
+		$AnimatedSprite2D.play("walk_down_unarmed")
 	
 	#Move up will need to be limited to surface of tilemap eventually
-	if Input.is_action_pressed("move_up"):
+	elif Input.is_action_pressed("move_up"):
 		position.y -= 1
+		$AnimatedSprite2D.play("walk_up_unarmed")
+	else:
+		$AnimatedSprite2D.stop()
+		
+		
+	
 		
 	
