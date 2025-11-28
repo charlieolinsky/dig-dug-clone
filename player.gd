d793e81138e3913f1291b3ec25e721a0df320fe6
Charles Olinsky <charlesolinsky@gmail.com>
2025-11-28 16:46:03 -0500

    player start facing right

--- player.gd
+++ player.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
 
+func _ready():
+	$AnimatedSprite2D.play("walk_right")
+
 func _process(_delta: float):
 	
 	#tackle this later
@@ -10,20 +13,22 @@ func _process(_delta: float):
 	#Movement
 	if Input.is_action_pressed("move_right"):
 		position.x += 1
-		$AnimatedSprite2D.play("walk_right_unarmed")
+		$AnimatedSprite2D.play("walk_right")
 	elif Input.is_action_pressed("move_left"):
 		position.x -= 1
-		$AnimatedSprite2D.play("walk_left_unarmed")
+		$AnimatedSprite2D.play("walk_left")
 	elif Input.is_action_pressed("move_down"):
 		position.y += 1
-		$AnimatedSprite2D.play("walk_down_unarmed")
+		$AnimatedSprite2D.play("walk_down")
 	
 	#Move up will need to be limited to surface of tilemap eventually
 	elif Input.is_action_pressed("move_up"):
 		position.y -= 1
-		$AnimatedSprite2D.play("walk_up_unarmed")
+		$AnimatedSprite2D.play("walk_up")
 	else:
 		$AnimatedSprite2D.stop()
+	
+	#position.clamp
 		
 		
 	
